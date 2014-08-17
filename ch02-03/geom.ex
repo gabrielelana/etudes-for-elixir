2a525749b6bfbe235b59a0843f0447755a53e2c5
gabriele.lana <gabriele.lana@cleancode.it>
2014-08-17 18:40:22 +0200

    Formatting change

--- ch02-03/geom.ex
+++ ch02-03/geom.ex
@@ -4,13 +4,11 @@ defmodule Geom do
   Functions to calculate areas of various shapes.
   """
 
-
   @spec area(number(), number()) :: number()
   @doc """
   Calculates the area of a rectangle given length and width
   """
-
-  def area(length \\ 1, width \\ 1) do
+  def area(length\\1, width\\1) do
     length * width
   end
 end
