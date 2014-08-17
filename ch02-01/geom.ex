54a9053106c0c3e9b6afca0346f24c30008e6050
gabriele.lana <gabriele.lana@cleancode.it>
2014-08-17 17:53:19 +0200

    Chapter 02

---
+++ ch02-01/geom.ex
@@ -0,0 +1,5 @@
+defmodule Geom do
+  def area(length, width) do
+    length * width
+  end
+end
---
+++ ch02-03/geom.ex
@@ -0,0 +1,16 @@
+defmodule Geom do
+  @vsn 0.1
+  @moduledoc """
+  Functions to calculate areas of various shapes.
+  """
+
+
+  @spec area(number(), number()) :: number()
+  @doc """
+  Calculates the area of a rectangle given length and width
+  """
+
+  def area(length \\ 1, width \\ 1) do
+    length * width
+  end
+end
