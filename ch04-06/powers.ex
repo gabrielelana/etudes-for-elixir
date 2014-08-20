6650606b0bea72d5facbb9df7996bb200a8fe58a
gabriele.lana <gabriele.lana@cleancode.it>
2014-08-20 18:11:34 +0200

    Chapter 04 - extra
    Use stream to solve the nth_root problem

---
+++ ch04-06/powers.ex
@@ -0,0 +1,28 @@
+defmodule Powers do
+
+  @doc """
+  Uses the Newton-Raphson method for calculating roots
+  Solved using infinite streams of guesses, stop when the difference between
+  the current guess and the next guess is less than wanted precision
+  """
+
+  @spec nth_root(number(), integer()) :: number()
+
+  def nth_root(x, n) when is_integer(n) do
+    guesses = nth_root_guesses(x, n)
+    {:ok, {v, _}} =
+      Stream.zip(guesses, guesses |> Stream.drop(1))
+      |> Stream.map(fn({g1, g2}) -> {g2, abs(g1-g2)} end)
+      |> Stream.drop_while(fn({_v, d}) -> d >= 1.0e-8 end)
+      |> Enum.fetch(0)
+    v
+  end
+
+  defp nth_root_guesses(x, n) do
+    f = &(:math.pow(&1, n) - x)
+    f_prime = &(n * :math.pow(&1, n - 1))
+    guesser = &(&1 - (f.(&1) / f_prime.(&1)))
+    start_at = x / 2.0
+    Stream.iterate(start_at, guesser)
+  end
+end
