941e93fa9e56dcb7afaf4f5c6b151e49c92977a3
gabriele.lana <gabriele.lana@cleancode.it>
2014-10-09 16:49:00 +0200

    Chapter 11-01: Part Two

--- ch11-01.exs
+++ ch11-01.exs
@@ -2,6 +2,13 @@
 # create an ETS table for the calls. The function that does this will be named
 # setup/1, and its argument will be the name of the file containing the data.
 
+# Write functions to summarize the number of minutes for a single phone number
+# (summary/1) or for all phone numbers. (summary/0). These functions return a
+# list of tuples in the form:
+
+# [{phoneNumber1, minutes]},{phoneNumber2, minutes}, …]
+
+
 defmodule Phone do
   require Record;
 
@@ -24,6 +31,42 @@ defmodule Phone.ETS do
 
   def lookup(number) do
     :ets.lookup(:calls, number)
+  end
+
+  def summary do
+    summary({:phone, :"_", :"_", :"_", :"_", :"_"})
+  end
+
+  def summary(number) when is_binary(number) do
+    summary({:phone, number, :"_", :"_", :"_", :"_"}) |> List.first
+  end
+
+  def summary(pattern) do
+    :ets.match_object(:calls, pattern)
+        |> Enum.group_by(fn({:phone, number, _, _, _, _}) -> number end)
+        |> Enum.map(
+            fn({number, calls}) ->
+              { number,
+                Enum.reduce(calls, 0,
+                  fn({:phone, _number, sd, st, ed, et}, minutes_of_conversation) ->
+                    minutes_of_conversation + minutes_between({sd, st}, {ed, et})
+                  end)
+              }
+            end)
+  end
+
+  defp minutes_between({sd, st}, {ed, et}) do
+    div(abs(datetime_to_seconds(ed, et) - datetime_to_seconds(sd, st)) + 59, 60)
+  end
+
+  defp datetime_to_seconds(date, time) do
+    [year,month,day] = String.split(date, "-")
+      |> Enum.map(fn(n) -> Regex.replace(~r/^0?/, n, "") end)
+      |> Enum.map(&String.to_integer/1)
+    [hour,minute,second] = String.split(time, ":")
+      |> Enum.map(fn(n) -> Regex.replace(~r/^0?/, n, "") end)
+      |> Enum.map(&String.to_integer/1)
+    :calendar.datetime_to_gregorian_seconds({{year, month, day}, {hour, minute, second}})
   end
 
   defp split(line) do
@@ -51,5 +94,23 @@ defmodule Phone.Test do
       {:phone, "650-555-3326", "2013-03-10", "09:05:48", "2013-03-10", "09:09:08"},
       {:phone, "650-555-3326", "2013-03-10", "09:10:12", "2013-03-10", "09:13:09"}
     ]
+  end
+
+  test "summary for all telephone numbers" do
+    Phone.ETS.setup("ch11-01-calls.csv")
+    assert (Phone.ETS.summary |> Enum.sort) == [
+      {"213-555-0172",9},
+      {"301-555-0433",12},
+      {"415-555-7871",7},
+      {"650-555-3326",11},
+      {"729-555-8855",6},
+      {"838-555-1099",9},
+      {"946-555-9760",3}
+    ]
+  end
+
+  test "summary for a telephone numbers" do
+    Phone.ETS.setup("ch11-01-calls.csv")
+    assert Phone.ETS.summary("213-555-0172") == {"213-555-0172",9}
   end
 end
